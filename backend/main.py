28474100c7b736983103d38b72696cba44058c57
Orestis Tsirakis <orestistsira@gmail.com>
2024-05-27 14:42:10 +0300

    Added all cacao properties

--- backend/main.py
+++ backend/main.py
@@ -7,6 +7,7 @@ from dotenv import load_dotenv
 
 from models import Playbook, PlaybookInDB
 
+
 load_dotenv()
 
 app = FastAPI()
--- backend/models.py
+++ backend/models.py
@@ -5,6 +5,47 @@ from datetime import datetime
 from enum import Enum
 
 
+class Playbook(BaseModel):
+    type: str
+    spec_version: str
+    id: str
+    name: str
+    description: str | None = None
+    playbook_types: List[str] | None = None
+    playbook_activities: List[str] | None = None
+    playbook_processing_summary: PlaybookProcessingSummary | None = None
+    created_by: str
+    created: datetime
+    modified: datetime
+    revoked: bool | None = None
+    valid_from: datetime | None = None
+    valid_until: datetime | None = None
+    derived_from: List[str] | None = None
+    related_to: List[str] | None = None
+    priority: int | None = None
+    severity: int | None = None
+    impact: int | None = None
+    industry_sectors: List[str] | None = None
+    labels: List[str] | None = None
+    external_references: List[ExternalReference] | None = None
+    markings: List[str] | None = None
+    playbook_variables: Dict[str, Variable] | None = None
+    workflow_start: str
+    workflow_exception: str | None = None
+    workflow: Dict[str, WorkflowStep] | None = None
+    playbook_extensions: Dict[str, Any] | None = None
+    authentication_info_definitions: Dict[str, AuthenticationInfo] | None = None
+    agent_definitions: Dict[str, AgentTarget] | None = None
+    target_definitions: Dict[str, AgentTarget] | None = None
+    extension_definitions: Dict[str, ExtensionDefinition] | None = None
+    data_marking_definitions: Dict[str, DataMarking] | None = None
+    signatures: List[Signature] | None = None
+
+
+class PlaybookInDB(Playbook):
+    mongo_id: str = Field(..., alias='_id')
+
+
 class ExternalReference(BaseModel):
     name: str
     description: str | None = None
@@ -47,6 +88,20 @@ class WorkflowStepType(str, Enum):
     switch_condition = 'switch-condition'
 
 
+class Command(BaseModel):
+    type: str
+    description: str | None = None
+    version: str | None = None
+    playbook_activity: str | None = None
+    external_references: List[ExternalReference] | None = None
+
+    command: str | None = None
+    command_b64: str | None = None
+    headers: Dict[str, str] | None = None
+    content: str | None = None
+    content_b64: str | None = None
+
+
 class WorkflowStep(BaseModel):
     type: WorkflowStepType
     name: str | None = None
@@ -59,6 +114,19 @@ class WorkflowStep(BaseModel):
     on_completion: str | None = None
     on_success: str | None = None
     on_failure: str | None = None
+    commands: List[Command] | None = None
+    agent: str | None = None
+    targets: List[str] | None = None
+    in_args: List[str] | None = None
+    out_args: List[str] | None = None
+    playbook_id: str | None = None
+    playbook_version: str | None = None
+    next_steps: List[str] | None = None
+    condition: str | None = None
+    on_true: str | None = None
+    on_false: str | None = None
+    switch: str | None = None
+    cases: Dict[str, str] | None = None
     step_extensions: Dict[str, Any] | None = None
 
 
@@ -68,6 +136,14 @@ class AuthenticationInfo(BaseModel):
     description: str | None = None
     authentication_info_extensions: Dict[str, Any] | None = None
 
+    username: str | None = None
+    user_id: str | None = None
+    password: str | None = None
+    oauth_header: str | None = None
+    token: str | None = None
+    kms: bool | None = None
+    kms_key_identifier: str | None = None
+	
 
 class CivicLocation(BaseModel):
     name: str | None = None
@@ -85,12 +161,35 @@ class CivicLocation(BaseModel):
     precision: str | None = None
 
 
+class Contact(BaseModel):
+    email: Dict[str, str] | None = None
+    phone: Dict[str, str] | None = None
+    contact_details: str | None = None
+
+
+class AddressType(str, Enum):
+    dname = 'dname',
+    ipv4 = 'ipv4',
+    ipv6 = 'ipv6',
+    l2mac = 'l2mac',
+    vlan = 'vlan',
+    url = 'url',
+
+
 class AgentTarget(BaseModel):
     type: str
     name: str
     description: str | None = None
     location: CivicLocation | None = None
     agent_target_extensions: Dict[str, Any] | None = None
+
+    contact: Contact | None = None
+    logical: List[str] | None = None
+    sector: str | None = None
+    address: Dict[AddressType, list[str]] | None = None
+    port: str | None = None
+    authentication_info: str | None = None
+    category: List[str] | None = None
 
 
 class ExtensionDefinition(BaseModel):
@@ -144,44 +243,3 @@ class Signature(BaseModel):
     thumbprint: str | None = None
     value: str
     signature: Signature | None = None
-
-
-class Playbook(BaseModel):
-    type: str = 'playbook'
-    spec_version: str = 'cacao-2.0'
-    id: str
-    name: str
-    description: str | None = None
-    playbook_types: List[str] | None = None
-    playbook_activities: List[str] | None = None
-    playbook_processing_summary: PlaybookProcessingSummary | None = None
-    created_by: str
-    created: datetime
-    modified: datetime
-    revoked: bool | None = None
-    valid_from: datetime | None = None
-    valid_until: datetime | None = None
-    derived_from: List[str] | None = None
-    related_to: List[str] | None = None
-    priority: int | None = None
-    severity: int | None = None
-    impact: int | None = None
-    industry_sectors: List[str] | None = None
-    labels: List[str] | None = None
-    external_references: List[ExternalReference] | None = None
-    markings: List[str] | None = None
-    playbook_variables: Dict[str, Variable] | None = None
-    workflow_start: str
-    workflow_exception: str | None = None
-    workflow: Dict[str, WorkflowStep] | None = None
-    playbook_extensions: Dict[str, Any] | None = None
-    authentication_info_definitions: Dict[str, AuthenticationInfo] | None = None
-    agent_definitions: Dict[str, Any] | None = None
-    target_definitions: Dict[str, Any] | None = None
-    extension_definitions: Dict[str, ExtensionDefinition] | None = None
-    data_marking_definitions: Dict[str, DataMarking] | None = None
-    signatures: List[Signature] | None = None
-
-
-class PlaybookInDB(Playbook):
-    mongo_id: str = Field(..., alias='_id')
