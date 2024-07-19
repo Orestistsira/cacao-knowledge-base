9449ac3d4296465f7f4bf2e403cea7e8c397161b
Orestis Tsirakis <orestistsira@gmail.com>
2024-07-19 21:28:10 +0300

    Configure CORS

--- backend/main.py
+++ backend/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from dotenv import load_dotenv
 from routers.playbooks import router as playbooks_router
 
@@ -6,6 +7,15 @@ from routers.playbooks import router as playbooks_router
 load_dotenv()
 
 app = FastAPI()
+
+# Configure CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Allows all origins
+    allow_credentials=True,
+    allow_methods=["*"],  # Allows all methods
+    allow_headers=["*"],  # Allows all headers
+)
 
 # Include playbooks router
 app.include_router(playbooks_router)
